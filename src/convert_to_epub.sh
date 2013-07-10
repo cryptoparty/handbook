77422aeb9f6acecf3a6a1154ac81f02f1c878533
samthetechie <samuelcarlisle@gmail.com>
2013-07-10 06:46:24 +0200

    added more automated SHA256 checksumming

--- src/convert_to_epub.sh
+++ src/convert_to_epub.sh
@@ -15,6 +15,7 @@ pandoc book.md -o book.epub
 
 #change the filename
 cp book.epub ../cryptoparty-handbook-`date +%F`.epub
+sha256sum ../cryptoparty-handbook-`date +%F`.epub >> ../SHA256SUM
 
 #rm ./book.md #commenting this out in order to leave a copy of the combined markdown in the /src directory
 rm ./book.epub
